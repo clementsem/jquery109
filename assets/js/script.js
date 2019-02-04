0125afa0b472962194197e6243e274de96846310
clementsem <clementctes@gmail.com>
2019-02-04 09:42:01 +0100

    exo9

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+
+  $('#firstText, #thirdText').css({"border-color": "green",
+                                  "border-width":"5px",
+                                  "border-style":"dashed"});
+
+})
